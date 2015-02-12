a4375023933d43bf30dbd49594c0be78c3a9d178
uFrost <Jcub142@gmail.com>
2015-02-11 19:40:09 -0800

    Create ChatPanel.java

---
+++ src/ChatPanel.java
@@ -0,0 +1,27 @@
+package UI;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+public class ChatPanel extends JPanel{
+
+	void UpdateChat(String newThing){
+		JTextArea text = new JTextArea();
+		text.setBackground(new Color(40, 40, 40));
+		text.setForeground(new Color(200, 200, 200));
+		
+		if(newThing.startsWith(FireBaseUtils.myName()){
+			text.setAlignmentX(RIGHT_ALIGNMENT);
+		}
+		else{
+			text.setAlignmentX(LEFT_ALIGNMENT);
+		}
+		
+		this.add(text);
+	}
+	
+	
+	
+}
