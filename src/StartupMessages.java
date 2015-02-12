bcac42675258e14b096d1cdcaf19c14b1b05e4ed
uFrost <Jcub142@gmail.com>
2015-02-11 19:42:13 -0800

    Create StartupMessages.java

---
+++ src/StartupMessages.java
@@ -0,0 +1,55 @@
+package UI;
+
+public class StartupMessages {
+
+
+	String[] StartupMessages = new String[]{
+			
+			
+			"\"I'd date me\" - Jacob Freeland",
+			"Where will you be when social anxiety strikes?",
+			"I can count to alone!",
+			"Is it that time of day already?",
+			"Please wait... Secretly farming data...",
+			"Who can deny your urges? Technically, everyone.",
+			"SWEEP THE LEG",
+			"Let's initialize a relationship...",
+			"Roses = Red; Violets = Blue; I'd like to start a repo with you.",
+			"Programmers Meet",
+			"Our variable types don't match, but I'm into that.",
+			"While(youStay){EnjoyYourself();}",
+			"Boolean on me!",
+			"Now 100% better than Jacob Golden-Needham's",
+			"Keep it in your pants! ;)",
+			"We won't - stop - you now!",
+			"I don't want to break; up",
+			"do{!nothing}while(withYou);",
+			"Improve your handwriting!",
+			"Press x to speak",
+			"Behind you!",
+			"You can't be THAT bad- oh.",
+			"Best in class",
+			"If(!male){swarm();}",
+			"Goto isn't even that bad!",
+			"I need less time on my hands...",
+			"Why aren't you working on your Mode Lode?",
+			"Don't talk to me!",
+			"Ew - Java",
+			"Making you inferior since yesterday!",
+			"Is there time to run?!",
+			"What we have here is a misunderstanding",
+			"I can't stop!",
+			"We don't need your stinkin' badges!",
+			"Another reason to leave the house.",
+			"Can you blame us? Yes.",
+			"Don't mess up.",
+			"Are you nervous?",
+			"You could be making money right now.",
+			"Sleep! Dang, didn't work.",
+			"White House: Totally hacked.",
+			"JET FUEL CAN'T BURN THROUGH STEEL",
+			"\"hereTheShitBe\" - I work too hard to be scolded",
+			"Legalize gay marijuana!"
+	};
+	
+}
